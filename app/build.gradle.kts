2aa59641cab79f0564b99d0715011e0ad0dd1caf
Nikita Ryazanov <ryazanov.na@yandex.ru>
2024-09-06 19:15:23 +0400

    add Dockerfile

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
-    id("java")
+    application
     checkstyle
+    id("java")
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("io.freefair.lombok") version "8.10"
 }
 
 group = "hexlet.code"
@@ -10,13 +13,26 @@ repositories {
     mavenCentral()
 }
 
+application {
+    mainClass = "hexlet.code.App"
+}
+
 checkstyle {
     toolVersion = "10.12.4"
 }
 
 dependencies {
+
+    // jupiter
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    // javalin
+    implementation("io.javalin:javalin:6.3.0")
+
+    // slf4j
+    implementation("org.slf4j:slf4j-simple:2.0.16")
+
 }
 
 tasks.test {
