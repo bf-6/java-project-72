13bb0a1b9cf6f3ab27c4bed5ef5c2aa0bde1148d
Nikita Ryazanov <ryazanov.na@yandex.ru>
2024-09-06 12:43:29 +0400

    README.md update

---
+++ app/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("java")
+    checkstyle
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+checkstyle {
+    toolVersion = "10.12.4"
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
